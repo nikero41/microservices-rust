fded5d028760ec464415e0abf080d2c785e4b538
Stavros Nikoletopoulos <snikoletopoulos@hotmail.com>
2026-05-26 00:51:22 +0200

    Remove type annotations

--- auth/src/main.rs
+++ auth/src/main.rs
@@ -6,17 +6,15 @@ mod sessions;
 mod users;
 
 use auth::*;
-use sessions::{Sessions, SessionsImpl};
-use users::{Users, UsersImpl};
+use sessions::SessionsImpl;
+use users::UsersImpl;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let addr = "[::0]:50051".parse()?;
 
-    let users_service: Box<Mutex<dyn Users + Send + Sync + 'static>> =
-        Box::new(Mutex::new(UsersImpl::default()));
-    let sessions_service: Box<Mutex<dyn Sessions + Send + Sync + 'static>> =
-        Box::new(Mutex::new(SessionsImpl::default()));
+    let users_service = Box::new(Mutex::new(UsersImpl::default()));
+    let sessions_service = Box::new(Mutex::new(SessionsImpl::default()));
 
     let auth_service = AuthService::new(users_service, sessions_service);
 
