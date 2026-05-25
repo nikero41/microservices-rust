5a0518dcba3f5866d3013a0fca3bae51d16e7091
Stavros Nikoletopoulos <snikoletopoulos@hotmail.com>
2026-05-26 00:11:30 +0200

    Add custom error for auth service

---
+++ auth/src/errors.rs
@@ -0,0 +1,11 @@
+use thiserror::Error;
+
+#[derive(Debug, Error)]
+pub enum AuthError {
+    #[error("Invalid credentials")]
+    InvalidCredentials,
+    #[error("Internal error")]
+    InternalError(String),
+    #[error("Username already exists")]
+    UsernameAlreadyExists,
+}
